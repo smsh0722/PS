869b67ae14c1402640f18bd4f9b665e911defaef
smsh0722 <smsh0722@gmail.com>
2022-03-02 21:52:55 +0900

    1305

---
+++ problems/1305.cpp
@@ -0,0 +1,37 @@
+/* smsh0722
+ * 1305
+ * 광고
+ * */
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main( void )
+{
+    int L; cin >> L;
+    char* board = new char[L+1]; 
+    cin >> board;
+
+    int* lps = new int[L];
+    lps[0] = 0;
+    {
+        int len = lps[0];
+        int i = 1;
+        while ( i < L ){
+            if ( board[i] == board[len] ){
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                if ( len == 0 )
+                    i++;
+                else 
+                    len = lps[len-1];
+            }
+        }
+    }
+
+    cout << L - lps[L-1];
+    return 0;
+}
