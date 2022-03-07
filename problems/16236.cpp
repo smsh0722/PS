385cbde0ecea020fcb94a2c63abc178dfc2ed625
smsh0722 <smsh0722@gmail.com>
2022-03-07 17:32:30 +0900

    16236

--- problems/16236.cpp
+++ problems/16236.cpp
@@ -1,114 +1,148 @@
 #include <iostream>
-#include <cstdlib>
-#include <vector>
-#include <set>
-
+#include <queue>
 using namespace std;
 
-int searchTarget( const int n, int loc[], int& pos, int& size )
-{
-    int dir[4] = { -n, n, -1, 1 };
-    int len = 0;
-    vector<int> queue[ n * n ];
-    queue[0].push_back(pos);
-    bool visited[n*n] = { false };
-    visited[pos] = true;
-    set<int> dst;
+struct point{
+    int r, c;
+    int dist;
+};
 
-    while ( len < n * n ){
-        int tmp;
-        for ( int idx = 0; idx < queue[len].size(); idx++ ){
-            tmp = queue[len].at(idx);
-            
-            for ( int i = 0; i < 4; i++ ){
-                int trg = tmp + dir[i];
-                if ( trg < 0 || trg >= n * n ) continue;
-                if ( i == 2 && tmp % n == 0 ) continue;
-                if ( i == 3 && trg % n == 0 ) continue;
-                
-                // cout << trg << "] "; // Debug
-                
-                if ( loc[trg] == 0 || loc[trg] == size ){
-                    if ( visited[trg] == false ) queue[len+1].push_back( trg );
-                    // cout << "len: " << len+1 << ", " << trg << "A\n"; // Deubg
-                }
-                else if ( loc[trg] > size ){
-                    //cout << "len: " << len+1 << ", " << trg << "B\n"; // Deubg
-                }
-                else { //loc[trg] < size
-                    // cout << "len: " << len+1 << ", " << trg << "C\n"; // Deubg
-                    dst.insert(trg);
-                }
-                visited[ trg ] = true;
-            }
-        }
-        /*
-        for ( int i = 0; i < queue[len+1].size();i++ ){
-            cout << "queue" << len+1 << ": " << queue[len+1][i] << "\n";
-        }// Debug
-        */
-        if ( dst.size() == 0 ) len++;
-        else break;
-    }
-    if ( dst.size() != 0 ){
-        int tmp = *dst.begin();
-        /*
-        for ( auto i = dst.begin(); i != dst.end(); i++ ){
-            cout << *i << " "; // Debug;
-        } cout << "\n";
-        */
-        loc[tmp] = 9;
-        loc[pos] = 0;
-        pos = tmp;
-        return (len + 1);
-    }
-    return 0;
-}
+void printMap( int** map, const int SIZE, int level, int exp, int time );
 
 int main( void )
 {
-    int sum = 0;
-    int n;
-    cin >> n;
-    {
-        int loc[n*n] = {0};
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-        int tmp;
-        int pos;
-        for ( int r = 0; r < n; r++ ){
-            for ( int c = 0; c < n; c++ ){
-                cin >> tmp;
-                if ( tmp == 9 ) {
-                    pos = r * n + c;
-                }
-                loc[r*n + c] = tmp;
-            }
-        }
-        
-        // search
-        int size = 2;
-        int count = 0;
-        while ( true ){
-            int time = searchTarget( n, loc, pos, size );
-            if ( time == 0 ) break;
-            /*
-            cout << "Time: " << time << "\n"; // Debug
-            for ( int r = 0; r < n; r++ ){
-                for ( int c = 0; c < n; c++ ){
-                    cout << loc[r*n+c] << " ";
-                }
-                cout << "\n";
-            } // Debug
-            */
-            sum += time;
-            count++;
-            if ( count == size ){
-                count = 0;
-                size++;
-            }
+    int N;
+    cin >> N;
+
+    int level = 2;
+    int exp = 0;
+    int time = 0;
+    queue<point> Q;
+    int** map;
+
+    map = new int*[N];
+    for ( int i = 0; i < N; i++ ){
+        map[i] = new int[N];
+        for ( int j = 0; j < N; j++ ){
+            cin >> map[i][j];
+            if ( map[i][j] == 9 )
+                Q.push( {i, j, 0} );
         }
     }
 
-    cout << sum;
+    int color[N][N];
+    for ( int i = 0; i < N; i++ ){
+        for ( int j = 0; j < N; j++ )
+            color[i][j] = -1;
+    }
+
+    point* curPoint = new point{ Q.front().r, Q.front().c, Q.front().dist };
+    point* nextPoint = nullptr;
+    int dist = 0;
+    while ( Q.empty() == false ){
+        point trgP = Q.front(); Q.pop();
+
+        dist += trgP.dist - dist;
+
+        // Check Eat
+        if ( nextPoint != nullptr && nextPoint->dist < trgP.dist ){
+            while (Q.empty() == false )
+                Q.pop();
+            
+            // Eat
+            map[curPoint->r][curPoint->c] = 0;
+            time += nextPoint->dist;
+            dist = 0;
+            nextPoint->dist = 0;
+            exp++;
+            if( level == exp ){
+                level++;
+                exp = 0;
+            }
+            delete curPoint;
+            curPoint = nextPoint;
+            nextPoint = nullptr;
+
+            Q.push( *curPoint );
+
+            map[curPoint->r][curPoint->c] = 9;
+
+            // printMap( map, N, level, exp, time ); // Debug
+            continue;
+        }
+
+        // Check Feed
+        if ( map[trgP.r][trgP.c] > 0 && map[trgP.r][trgP.c] < level && map[trgP.r][trgP.c] <= 6 ){
+            point* tmp = new point;
+            tmp->r = trgP.r; tmp->c = trgP.c; tmp->dist = trgP.dist;
+            if ( nextPoint == nullptr )
+                nextPoint = tmp;
+            else if ( nextPoint->r > trgP.r )
+                nextPoint = tmp;
+            else if ( nextPoint->r == trgP.r && nextPoint->c > trgP.c )
+                nextPoint = tmp;
+        }
+
+        // Push Queue
+        int candidateP[4][2]; // 4x(r, c)
+        candidateP[0][0] = trgP.r + 1; candidateP[0][1] = trgP.c;
+        candidateP[1][0] = trgP.r - 1; candidateP[1][1] = trgP.c;
+        candidateP[2][0] = trgP.r;     candidateP[2][1] = trgP.c + 1;
+        candidateP[3][0] = trgP.r;     candidateP[3][1] = trgP.c - 1;
+        for ( int i = 0; i < 4; i++ ){
+            if ( candidateP[i][0] < 0 || candidateP[i][0] >= N )
+                continue;
+            if ( candidateP[i][1] < 0 || candidateP[i][1] >= N )
+                continue;
+            int r = candidateP[i][0]; int c = candidateP[i][1];
+            if ( map[r][c] <= 6 && map[r][c] <= level && color[r][c] < time){
+                Q.push( {r, c, dist+1} );
+                color[r][c] = time;
+            }
+        }
+
+        if ( Q.empty() == true && nextPoint != nullptr ){
+            while ( Q.empty() == false )
+                Q.pop();
+
+            // Eat
+            map[curPoint->r][curPoint->c] = 0;
+            time += nextPoint->dist;
+            dist = 0;
+            nextPoint->dist = 0;
+            exp++;
+            if( level == exp ){
+                level++;
+                exp = 0;
+            }
+            delete curPoint;
+            curPoint = nextPoint;
+            nextPoint = nullptr;
+
+            Q.push( *curPoint );
+
+            map[curPoint->r][curPoint->c] = 9;
+            
+            // printMap( map, N, level, exp, time ); // Debug
+            continue;
+        }
+    }
+
+    cout << time;
+
     return 0;
+}
+
+void printMap( int** map, const int SIZE, int level, int exp, int time )
+{
+    cout << "\nlevel: " << level 
+            << ", exp: " << exp
+            << ", time: " << time << "\n";
+    for ( int i = 0; i < SIZE; i++ ){
+        for ( int j = 0; j < SIZE; j++ )
+            cout << map[i][j] << " ";
+        cout << "\n";
+    }
 }
