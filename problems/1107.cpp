ae7dbfec205de86e0d74f804ca5b1ccd7efe110a
smsh0722 <smsh0722@gmail.com>
2022-03-09 11:10:20 +0900

    1107.cpp

--- problems/1107.cpp
+++ problems/1107.cpp
@@ -1,41 +1,69 @@
+/* smsh0722
+ * 1107
+ * 리모컨
+ * */
 #include <iostream>
+#include <string>
 using namespace std;
 
-int count = 500001;
-const int UP = 1;
-const int Down = -1;
-bool button[10] = { 0 }; // true = disable
+const int INF = 500001; // 500,000
+enum condition{ 
+    WORKING = 0, 
+    NOTWORKING
+};
 
-void findMin( const int arr[], int Nth, int way );
+int findMinCase( string N, condition buttons[], int trgSize, string num );
 
 int main( void )
 {
-    int targetArr[6] = {0};
-    int target; cin >> target;
+    int N, M;
+    condition buttons[10] = { WORKING };
 
-    // set button
-    int temp;
-    int buttonNum; cin >> buttonNum;
-    for ( int i = 0; i < buttonNum; i++ ){
-        cin >> temp;
-        button[temp] = true;
+    cin >> N;
+    cin >> M;
+    for ( int i = 0; i < M; i++ ){
+        int button;
+        cin >> button;
+        buttons[button] = NOTWORKING;
     }
     
-    int Nth = 1;
-    targetArr[0] = temp % 10;
-    while ( temp >= 10 ){
-        temp /= 10;
-        targetArr[Nth] = temp % 10;
-        Nth++;
+    int a = abs( N - 100 ); // except numButton
+    int b = INF;            // Include numButton
+    {
+        string N_str = to_string( N );
+        int tmp;
+        // Case num length 1 ~ 
+        for ( int i = 1; i <= N_str.length() + 1; i++ ){
+            tmp = findMinCase( N_str, buttons, i, "" );
+            b = b > tmp ? tmp : b;
+        }
+        // Case num 0
+        if ( buttons[0] == WORKING ){
+            tmp = N - 0 + 1;
+            b = b > tmp ? tmp : b;
+        }
     }
 
-    findMin( targetArr, Nth, UP );
-    findMin( targetArr, Nth, Down );
+    cout << (a > b ? b : a) << endl;
 
-    cout << count << endl;
     return 0;
 }
 
-void findMin( const int arr[], int Nth, int way ){
+int findMinCase( string N, condition buttons[], int trgSize, string num )
+{   
+    // End
+    if ( num.length() == trgSize )
+        return abs( stoi(N) - stoi(num) ) + trgSize;
     
+    // Click more button
+    int min = INF;
+    for ( int i = 0; i <= 9; i++ ){
+        if ( i == 0 && num.length() == 0 )
+            continue;
+        if ( buttons[i] == WORKING ){
+            int tmp = findMinCase( N, buttons, trgSize, num + to_string(i) );
+            min = min > tmp ? tmp : min;
+        }
+    }
+    return min;
 }
