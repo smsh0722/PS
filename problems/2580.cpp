bcadd39ea0afdf0ae157b49bf6ae28df13d7b85c
smsh0722 <smsh0722@gmail.com>
2022-03-24 22:02:11 +0900

    2580

--- problems/2580.cpp
+++ problems/2580.cpp
@@ -1,38 +1,124 @@
+/* smsh0722
+ * 2580
+ * 스도쿠
+ * */
 #include <iostream>
-#include <vector>
 using namespace std;
 
-const int LINESIZE = 9;
+int board[9][9];
 
-bool fillBoard( vector< vector<int> >& board, vector< pair<int, int> >& blankVector );
+bool solve_sudoku( int blanks[81][2], int blanks_size, int idx );
+bool checkBox( int curR, int curC );
+bool checkRow( int curR );
+bool checkColumn( int curC );
 
 int main( void )
 {
-    ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
-    vector< vector<int> > board( LINESIZE, vector<int>(LINESIZE) );
-    vector< pair<int, int> > blankVector; // <r, c>
-    for ( int r = 0; r < LINESIZE; r++ ){
-        for ( int c = 0; c < LINESIZE; c++ ){
+    int blanks[81][2];
+    int blanks_size = 0;
+
+    for ( int r = 0; r < 9; r++ ){
+        for ( int c = 0; c < 9; c++ ){
             cin >> board[r][c];
+
             if ( board[r][c] == 0 ){
-                blankVector.push_back( pair<int, int>( r, c ) );
+                blanks[blanks_size][0] = r;
+                blanks[blanks_size][1] = c;
+                blanks_size++;
             }
         }
     }
-    
-    fillBoard( board, blankVector );
 
-    for ( int r = 0; r < LINESIZE; r++ ){
-        for ( int c = 0; c < LINESIZE; c++ ){
+    // solve
+    solve_sudoku( blanks, blanks_size, 0 );
+
+    // Print result
+    for ( int r = 0; r < 9; r++ ){
+        for ( int c = 0; c < 9; c++ )
             cout << board[r][c] << " ";
-        }
         cout << "\n";
     }
-    return 0;
 }
 
-bool fillBoard( vector< vector<int> >& board, vector< pair<int, int> >& blankVector )
+bool solve_sudoku( int blanks[81][2], int blanks_size, int idx )
 {
+    int curR = blanks[idx][0];
+    int curC = blanks[idx][1];
+
+    for ( int i = 1; i <= 9; i++ ){
+        board[curR][curC] = i; // Temp ans
+
+        // Check tmp answer
+        if ( checkBox( curR, curC ) == false )
+            continue;
+        if ( checkRow( curR ) == false )
+            continue;
+        if ( checkColumn( curC ) == false )
+            continue;
+        
+        // Solve next Blanks
+        if ( idx + 1 < blanks_size && solve_sudoku( blanks, blanks_size, idx + 1 ) == true )
+            return true;
+        else if ( idx + 1 == blanks_size )
+            return true;
+    }
+
+    board[curR][curC] = 0; // Reset to 0
+
+    return false;
+}
+bool checkBox( int curR, int curC )
+{
+    int sR = (curR / 3)*3; // start row {0, 3, 6}
+    int sC = (curC / 3)*3; // start Column {0, 3, 6}
+
+    bool flag[10] = {0};
+    for ( int r = 0; r < 3; r++ ){
+        for ( int c = 0; c < 3; c++ ){
+            int trgVal = board[sR+r][sC+c];
+
+            if ( trgVal == 0 )
+                continue;
+            if ( flag[trgVal] == true )
+                return false;
+
+            flag[trgVal] = true;
+        }
+    }
+
+    return true;
+}
+bool checkRow( int curR )
+{
+    bool flag[10] = {false};
+    for ( int c = 0; c < 9; c++ ){
+        int trgVal = board[curR][c];
+
+        if ( trgVal == 0 )
+            continue;
+        if ( flag[trgVal] == true )
+            return false;
+        
+        flag[trgVal] = true;
+    }
+    return true;
+}
+bool checkColumn( int curC )
+{
+
+    bool flag[10] = {false};
+    for ( int r = 0; r < 9; r++ ){
+        int trgVal = board[r][curC];
+
+        if ( trgVal == 0 )
+            continue;
+        if ( flag[trgVal] == true )
+            return false;
+        
+        flag[trgVal] = true;
+    }
+
     return true;
 }
