2752db81b2160b5e3036338795b3bc4729fc6ed6
smsh0722 <smsh0722@gmail.com>
2022-04-11 12:12:53 +0900

    Optimize 1761

--- problems/1761.cpp
+++ problems/1761.cpp
@@ -1,7 +1,3 @@
-/* smsh0722
- * 1761
- * 정점들의 거리
- * */
 #include <iostream>
 #include <cstring>
 #include <cmath>
@@ -9,16 +5,21 @@ using namespace std;
 
 int INF = (-1)^(1<<31);
 struct edge {
-    int nodeNum;
+    int trgNode;
     int dist;
     edge* nextEdge;
 };
-edge** adjList;
 
+edge** adjList;         // Adjacency List
+int* distFromRoot;      // Distance(weight) of each Node from root
+int* firstOccurOfNode; // First occurrence of each Node
+int* dfsLog;         // Node at each occurrence
+int* levelOfNode;    // Level of each Node
+int* ST;             // Segment Tree
 
-void DFS( bool* visited, int curNode, int* route, int* nodeNumHash, int* distFromRoot, int* count );
-int constructSTUtil( int* ST, int idx, int l, int r, int* route, int* nodeNumHash );
-int getMinST( int* ST, int idx, int l, int r, int trgL, int trgR );
+void DFS( bool* visited, int curNode, int* count, int curLevel );
+int constructSTUtil( int idx, int l, int r );
+int getMinST( int idx, int l, int r, int trgL, int trgR );
 
 int main( void )
 {
@@ -27,113 +28,108 @@ int main( void )
     cin >> N;
 
     // Input
-
-    adjList = new edge*[N+1];
     {
+        adjList = new edge*[N+1];
         for ( int i = 0; i <= N; i++ )
             adjList[i] = nullptr;
 
-        int a, b, c;
+        int nodeA, nodeB, dist;
         for ( int i = 0; i < N - 1; i++ ){
-            cin >> a >> b >> c;
-            edge* a2b = new edge{ b, c, adjList[a] };
-            edge* b2a = new edge{ a, c, adjList[b] };
-            adjList[a] = a2b;
-            adjList[b] = b2a;
+            cin >> nodeA >> nodeB >> dist;
+            edge* a2b = new edge{ nodeB, dist, adjList[nodeA] };
+            edge* b2a = new edge{ nodeA, dist, adjList[nodeB] };
+            adjList[nodeA] = a2b;
+            adjList[nodeB] = b2a;
         }
     }
 
     // Pre-process
+    // DFS
+    {
+        distFromRoot = new int[N+1];
+        memset( distFromRoot, 0, sizeof(int)*(N+1) );
+        firstOccurOfNode = new int[N+1];
+        dfsLog = new int[2*N-1];
+        levelOfNode = new int[N+1];
 
-    int* distFromRoot = new int[N+1];
-    memset( distFromRoot, 0, sizeof(int)*(N+1) );
+        int count = 0;
 
-    int* nodeNumHash = new int[N+1];
-    int* nodeNumHash_reverse = new int[2*N-1];
-    int* ST;
+        bool* visited = new bool[N+1];
+        memset( visited, false, sizeof(bool)*(N+1) );
+
+        DFS( visited, 1, &count, 1 );
+
+        delete[] visited;
+    }
+    // Construct ST
     {
         int h = ceil( log2(2*N - 1) );
         int size = (1<<(h+1))-1;
         ST = new int[size];
         
-        int* route = new int[2*N-1];
-        int count = 0;
-        bool* visited = new bool[N+1];
-        memset( visited, false, sizeof(bool)*(N+1) );
-        DFS( visited, 1, route, nodeNumHash, distFromRoot, &count );
-
-        constructSTUtil( ST, 0, 0, 2*N-1-1, route, nodeNumHash );
-
-        memset( nodeNumHash_reverse, -1, sizeof(int)*(2*N-1));
-        for ( int i = 1; i <= N; i++ )
-            nodeNumHash_reverse[ nodeNumHash[i] ] = i;
-        
-        delete[] route;
-        delete[] visited;
+        constructSTUtil( 0, 0, 2*N-1-1 );
     }
 
     // Answer Qeury
-    int M;
-    cin >> M;
-    for ( int i = 0; i < M; i++ ){
+    {
+        int M;
+        cin >> M;
+
         int a, b;
-        cin >> a >> b;
-        if ( nodeNumHash[a] > nodeNumHash[b] ){
-            int tmp = a;
-            a = b;
-            b = tmp;
+        for ( int i = 0; i < M; i++ ){
+            cin >> a >> b;
+            if ( firstOccurOfNode[a] > firstOccurOfNode[b] ){
+                int tmp = a;
+                a = b;
+                b = tmp;
+            }
+
+            int lca = dfsLog[ getMinST( 0, 0, 2*N-1-1, firstOccurOfNode[a], firstOccurOfNode[b] ) ];
+
+            cout << (distFromRoot[a] + distFromRoot[b] - 2*distFromRoot[lca]) << "\n";
         }
-        int lca = nodeNumHash_reverse[ getMinST( ST, 0, 0, 2*N-1-1, nodeNumHash[a], nodeNumHash[b] ) ];
-        /*
-        {// DEbug
-        cout << distFromRoot[a] << " + "
-             << distFromRoot[b] << " - "
-             << "2*" << distFromRoot[lca] << " = ";
-        }
-        */
-        cout << (distFromRoot[a] + distFromRoot[b] - 2*distFromRoot[lca]) << "\n";
     }
+    
     return 0;
 }
-void DFS( bool* visited, int curNode, int* route, int* nodeNumHash, int* distFromRoot, int* count )
+void DFS( bool* visited, int curNode, int* count, int curLevel )
 {
-    route[*count] = curNode; // Set Route
-    nodeNumHash[curNode] = *count; // Set nodeNumHash
+    firstOccurOfNode[curNode] = *count; // Set firstOccurOfNode
+    dfsLog[(*count)++] = curNode; // Set dfsLog
+    levelOfNode[curNode] = curLevel; // Set Level to curNode
     visited[curNode] = true;
 
     // DFS
     edge* curEdge = adjList[curNode];
     while ( curEdge != nullptr ){
-        if ( visited[curEdge->nodeNum] == true ){
+        if ( visited[curEdge->trgNode] == true ){
             curEdge = curEdge->nextEdge;
             continue;
         }
-        *count += 1;
 
-        distFromRoot[curEdge->nodeNum] = distFromRoot[curNode] + curEdge->dist; // set Dist
-        DFS( visited, curEdge->nodeNum, route, nodeNumHash, distFromRoot, count ); // DFS
-        route[*count] = curNode;
+        distFromRoot[curEdge->trgNode] = distFromRoot[curNode] + curEdge->dist; // set Dist
+        DFS( visited, curEdge->trgNode, count, curLevel + 1 ); // DFS
+        dfsLog[(*count)++] = curNode;
 
         curEdge = curEdge->nextEdge;
     }
-    (*count)++;
 }
-int constructSTUtil( int* ST, int idx, int l, int r, int* route, int* nodeNumHash )
+int constructSTUtil( int idx, int l, int r )
 {
     // Base Case
     if ( l == r ){
-        ST[idx] = nodeNumHash[ route[l] ];
-        return ST[idx];
+        ST[idx] = l;
+        return l;
     }
     // Recursive Find
     int mid = ( r - l ) / 2 + l;
-    int lval = constructSTUtil( ST, idx*2+1, l, mid, route, nodeNumHash );
-    int rval = constructSTUtil( ST, idx*2+2, mid+1, r, route, nodeNumHash );
+    int lval = constructSTUtil( idx*2+1, l, mid );
+    int rval = constructSTUtil( idx*2+2, mid+1, r );
 
-    ST[idx] = lval < rval ? lval : rval;
+    ST[idx] = levelOfNode[dfsLog[lval]] < levelOfNode[dfsLog[rval]] ? lval : rval;
     return ST[idx];
 }
-int getMinST( int* ST, int idx, int l, int r, int trgL, int trgR )
+int getMinST( int idx, int l, int r, int trgL, int trgR )
 {
     // In
     if ( trgL <= l && r <= trgR )
@@ -144,8 +140,13 @@ int getMinST( int* ST, int idx, int l, int r, int trgL, int trgR )
     
     // Partly In
     int mid = ( r - l ) / 2 + l;
-    int lval = getMinST( ST, idx*2+1, l, mid, trgL, trgR );
-    int rval = getMinST( ST, idx*2+2, mid+1, r, trgL, trgR );
+    int lval = getMinST( idx*2+1, l, mid, trgL, trgR );
+    int rval = getMinST( idx*2+2, mid+1, r, trgL, trgR );
 
-    return ( lval < rval ? lval : rval );
+    if ( lval == INF )
+        return rval;
+    if ( rval == INF )
+        return lval;
+    
+    return ( levelOfNode[dfsLog[lval]] < levelOfNode[dfsLog[rval]] ? lval : rval );
 }
